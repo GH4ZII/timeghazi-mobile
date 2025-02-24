80c879aad500abcb6209baccd8b081d7aa8cb4e7
Ghazi Fida <ghazifa@uia.no>
2025-02-24 17:13:05 +0100

    Edited the API URL

---
+++ app/AuthHandler.tsx
@@ -0,0 +1,28 @@
+﻿import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
+import { ActivityIndicator } from "react-native";
+
+export const useAuth = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            const token = await AsyncStorage.getItem("token"); // 🔐 Hent token fra lagring
+            if (!token) {
+                router.replace("/login"); // ❌ Hvis ingen token, send til login-siden
+            } else {
+                setIsAuthenticated(true); // ✅ Hvis token finnes, brukeren er innlogget
+            }
+        };
+
+        checkAuth();
+    }, []);
+
+    return isAuthenticated;
+};
+
+export const AuthLoader = () => {
+    return <ActivityIndicator size="large" />;
+};
